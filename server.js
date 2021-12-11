442e7dd931df13c140494d367b70c8552b8f702a
Tyler Termini <tntermini@gmail.com>
2021-12-10 17:01:15 -0800

    init

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ app.get('/', (req, res) => {
 });
 
 
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
