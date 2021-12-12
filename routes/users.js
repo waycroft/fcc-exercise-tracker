ca4fa9f632b9c36b25ba2d399276a62fe86c5b24
Tyler Termini <tntermini@gmail.com>
2021-12-11 16:56:07 -0800

    fixes/comments

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,24 @@ router.get('/', async (req, res, next) => {
     res.send(users);
 })
 
+router.get('/:id/logs', async (req, res, next) => {
+    let exerciseLog = await User.aggregate(
+        [
+            { $addFields: {
+                count: {$size: "$log"}
+            }},
+            { $unset: ["__v"]} // mIGHT need to remove the _id that comes with each exercise. not sure why this is even adding...?
+        ])
+    res.send(exerciseLog);
+})
+
 router.post('/', urlParser, async (req, res, next) => {
     let user = new User({
         username: req.body.username
     })
     await user.save();
 
-    res.send(user);
+    res.send({_id: user._id, username: user.username});
 })
 
 router.post('/:id/exercises', urlParser, async (req, res, next) => {
@@ -33,6 +44,7 @@ router.post('/:id/exercises', urlParser, async (req, res, next) => {
 
     console.log(writeOp);
 
+    // don't like the two roundtrips to mongo but it'll do for now...
     let returnedUser = await User.findById(req.params.id, '-log -__v').lean();
     returnedUser.description = req.body.description;
     returnedUser.duration = Number(req.body.duration);
